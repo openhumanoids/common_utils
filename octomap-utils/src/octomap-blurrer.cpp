b0d488671576656fdf4c126d0e63b79436f0e207
abachrac <abachrac@d030852e-545f-40dc-8c57-637194663845>
2011-12-12 22:21:04 +0000

    add the blur radius to the blurred map's filename

--- octomap-utils/src/octomap-blurrer.cpp
+++ octomap-utils/src/octomap-blurrer.cpp
@@ -1,5 +1,6 @@
 #include "octomap_util.hpp"
 #include <bot_core/bot_core.h>
+#include <sstream>
 using namespace std;
 using namespace octomap;
 using namespace occ_map;
@@ -7,15 +8,22 @@ using namespace occ_map;
 int main(int argc, char ** argv)
 {
 
-  if (argc != 3) {
+  double blur_sigma = .5;
+  if (argc <2 || argc>3) {
     printf("Usage:\n");
     printf("%s <octomap_fname> <blur_radius>\n", argv[0]);
     exit(1);
   }
 
+  if (argc>=3)
+    blur_sigma = atof(argv[2]);
+
   string octomap_fname = argv[1];
-  double blur_sigma = atof(argv[2]);
-  string blurred_fname = octomap_fname + string("_blurred");
+
+
+  std::stringstream s;
+  s <<octomap_fname << "_blurred_" << blur_sigma;
+  string blurred_fname = s.str();
 
   printf("loading octomap from: %s\n", octomap_fname.c_str());
   octomap::OcTree * ocTree = new  octomap::OcTree(octomap_fname);
