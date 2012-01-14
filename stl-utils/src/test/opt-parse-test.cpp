a46e0ada3edf4476e46d6b072d266f96d8cbcf16
abachrac <abachrac@d030852e-545f-40dc-8c57-637194663845>
2012-01-14 07:28:32 +0000

    support printing strings

--- stl-utils/src/test/opt-parse-test.cpp
+++ stl-utils/src/test/opt-parse-test.cpp
@@ -11,13 +11,15 @@ int main(int argc, char ** argv)
   int i = -9;
   bool b = false;
   float f = -9;
+  std::string s = "asdf";
   parser.add("i", "iiii", "ayyy maytey", i, true);
   parser.add("b", "bbbbasdfasdf", "bzzz", b);
   parser.add("f", "ffff", "fpfppff", f);
+  parser.add("s", "ssss", "strings?", s);
 
   std::list<std::string> remaining = parser.parse();
 
-  std::cerr << "i:" << i << " b:" << b << " f:" << f << "\n";
+  std::cerr << "i:" << i << " b:" << b << " f:" << f << " s:" << s << "\n";
   std::cerr << "Remaining: ";
   for (std::list<std::string>::iterator it = remaining.begin(); it != remaining.end(); it++) {
     std::cerr << *it << " ";
