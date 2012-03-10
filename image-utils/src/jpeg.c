02fac5207d3fe4f747b72bd2d0eb695fdb5e3b2b
abachrac <abachrac@d030852e-545f-40dc-8c57-637194663845>
2012-03-10 14:23:40 +0000

    Revert Will's Commit

--- image-utils/src/jpeg.c
+++ image-utils/src/jpeg.c
@@ -31,55 +31,42 @@ term_source (j_decompress_ptr cinfo)
 {
 }
 
-int
-jpeg_decompress_to_8u_rgb (const uint8_t * src, int src_size,
+static void 
+jpeg_err_emit_message(j_common_ptr cinfo, int msg_level)
+{
+    // suppress warnings and errors
+}
+
+static int
+jpeg_decompress_8u (const uint8_t * src, int src_size,
+        uint8_t * dest, int width, int height, int stride, J_COLOR_SPACE ocs);
+
+int 
+jpeg_decompress_8u_rgb (const uint8_t * src, int src_size,
         uint8_t * dest, int width, int height, int stride)
 {
-    struct jpeg_decompress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-    struct jpeg_source_mgr jsrc;
+    return jpeg_decompress_8u (src, src_size, dest, width, height,
+            stride, JCS_RGB);
+}
 
-    cinfo.err = jpeg_std_error (&jerr);
-    jpeg_create_decompress (&cinfo);
-
-    jsrc.next_input_byte = src;
-    jsrc.bytes_in_buffer = src_size;
-    jsrc.init_source = init_source;
-    jsrc.fill_input_buffer = fill_input_buffer;
-    jsrc.skip_input_data = skip_input_data;
-    jsrc.resync_to_restart = jpeg_resync_to_restart;
-    jsrc.term_source = term_source;
-    cinfo.src = &jsrc;
-
-    jpeg_read_header (&cinfo, TRUE);
-    cinfo.out_color_space = JCS_RGB;
-    jpeg_start_decompress (&cinfo);
-
-    if (cinfo.output_height != height || cinfo.output_width != width) {
-        fprintf (stderr, "Error: Buffer was %dx%d but JPEG image is %dx%d\n",
-                width, height, cinfo.output_width, cinfo.output_height);
-        jpeg_destroy_decompress (&cinfo);
-        return -1;
-    }
-
-    while (cinfo.output_scanline < height) {
-        uint8_t * row = dest + cinfo.output_scanline * stride;
-        jpeg_read_scanlines (&cinfo, &row, 1);
-    }
-    jpeg_finish_decompress (&cinfo);
-    jpeg_destroy_decompress (&cinfo);
-    return 0;
+int 
+jpeg_decompress_8u_gray (const uint8_t * src, int src_size,
+        uint8_t * dest, int width, int height, int stride)
+{
+    return jpeg_decompress_8u (src, src_size, dest, width, height,
+            stride, JCS_GRAYSCALE);
 }
 
 int
-jpeg_decompress_to_8u_gray (const uint8_t * src, int src_size,
-        uint8_t * dest, int width, int height, int stride)
+jpeg_decompress_8u (const uint8_t * src, int src_size,
+        uint8_t * dest, int width, int height, int stride, J_COLOR_SPACE ocs)
 {
     struct jpeg_decompress_struct cinfo;
     struct jpeg_error_mgr jerr;
     struct jpeg_source_mgr jsrc;
 
     cinfo.err = jpeg_std_error (&jerr);
+    jerr.emit_message = jpeg_err_emit_message;
     jpeg_create_decompress (&cinfo);
 
     jsrc.next_input_byte = src;
@@ -92,8 +79,7 @@ jpeg_decompress_to_8u_gray (const uint8_t * src, int src_size,
     cinfo.src = &jsrc;
 
     jpeg_read_header (&cinfo, TRUE);
-
-    cinfo.out_color_space = JCS_GRAYSCALE;
+    cinfo.out_color_space = ocs;
     jpeg_start_decompress (&cinfo);
 
     if (cinfo.output_height != height || cinfo.output_width != width) {
@@ -244,7 +230,7 @@ jpeg_compress_8u_bgra (const uint8_t * src, int width, int height, int stride,
 
 #if 0
 int
-jpeg_decompress_to_8u_rgb_IPP (const uint8_t * src, int src_size,
+jpeg_decompress_8u_rgb_IPP (const uint8_t * src, int src_size,
         uint8_t * dest, int width, int height, int stride)
 {
     struct jpeg_decompress_struct cinfo;
@@ -446,7 +432,7 @@ jpeg_compress_8u_rgb_IPP (const uint8_t * src, int width, int height,
 
 
 int
-jpeg_dimensions (const uint8_t * src, int src_size, int *width, int *height)
+jpeg_get_dimensions (const uint8_t * src, int src_size, int *width, int *height)
 {
     struct jpeg_decompress_struct cinfo;
     struct jpeg_error_mgr jerr;
@@ -472,6 +458,7 @@ jpeg_dimensions (const uint8_t * src, int src_size, int *width, int *height)
 
     *width = cinfo.output_width;
     *height = cinfo.output_height;
+    //    *cs = cinfo.out_color_space; //TODO: should get the color space info...
     jpeg_destroy_decompress (&cinfo);
     return 0;
 }
