00dc40359031541141cf785ff70a20685ed57ac6
abry <abry@d030852e-545f-40dc-8c57-637194663845>
2012-06-12 05:03:04 +0000

    added c++ lcm type support

--- lcm-utils/src/fix-event-time.cpp
+++ lcm-utils/src/fix-event-time.cpp
@@ -7,10 +7,47 @@
 
 using namespace std;
 
+class lcmLogEventData: public lcm::LogEvent {
+public:
+  lcmLogEventData(const lcm::LogEvent & event) :
+      lcm::LogEvent(event)
+  {
+    this->data = malloc(this->datalen);
+    memcpy(this->data, event.data, this->datalen);
+  }
+
+  lcmLogEventData(const lcmLogEventData & other)
+  {
+    this->channel = other.channel;
+    this->datalen = other.datalen;
+    this->eventnum = other.eventnum;
+    this->timestamp = other.timestamp;
+
+    this->data = malloc(this->datalen);
+    memcpy(this->data, other.data, this->datalen);
+  }
+
+  ~lcmLogEventData()
+  {
+    free(this->data);
+  }
+
+  lcm::LogEvent getOr()
+  {
+    lcm::LogEvent orig;
+    orig.channel = this->channel;
+    orig.data = this->data;
+    orig.datalen = this->datalen;
+    orig.eventnum = this->eventnum;
+    orig.timestamp = this->timestamp;
+    return orig;
+  }
+};
+
 int main(int argc, char** argv)
 {
 
-  string poseChannel = "POSE";
+  string poseChannel = "STATE_ESTIMATOR_POSE";
   string logfileName;
   string outFileName;
   ConciseArgs opt(argc, argv, "logFname outFname");
@@ -31,26 +68,29 @@ int main(int argc, char** argv)
 
   lcm::LogFile out_log(outFileName, "w");
 
-  list<lcm::LogEvent> eventList = list<lcm::LogEvent>();
+  list<lcmLogEventData> eventList = list<lcmLogEventData>();
 
   while (1) {
     // Read a log event.
     const lcm::LogEvent *event = log.readNextEvent();
     if (!event)
       break;
-    eventList.push_back(*event);
+    eventList.push_back(lcmLogEventData(*event));
     if (event->channel == poseChannel) {
       bot_core::pose_t pmsg;
       pmsg.decode(event->data, 0, event->datalen);
       int64_t pose_utime = pmsg.utime;
-      list<lcm::LogEvent>::iterator event_it;
+      list<lcmLogEventData>::iterator event_it;
       for (event_it = eventList.begin(); event_it != eventList.end(); event_it++) {
         event_it->timestamp = pose_utime;
-        out_log.writeEvent(&(*event_it));
+        lcm::LogEvent orig = event_it->getOr();
+        out_log.writeEvent(&orig);
       }
       eventList.clear();
     }
   }
+
+  fprintf(stderr, "all done!\n");
 
   return 0;
 }
