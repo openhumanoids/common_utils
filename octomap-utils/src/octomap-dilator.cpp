2d0dd1c43e2cb9bdf6251864c8f6c56155e5b380
car <car@d030852e-545f-40dc-8c57-637194663845>
2012-09-04 21:24:25 +0000

    changed octomap dilator to accept input arguments to set bounds on map size for pruning the far reaches of a map that you don't want to keep

--- octomap-utils/src/octomap-dilator.cpp
+++ octomap-utils/src/octomap-dilator.cpp
@@ -15,10 +15,23 @@ void save_map(OcTree * ocTree, const string & outFname)
 int main(int argc, char **argv)
 {
   double radius = .5;
+  double xmin = -1;
+  double xmax = -1;
+  double ymin = -1;
+  double ymax = -1;
+  double zmin = -1;
+  double zmax = -1;
+
   string octomap_fname = "octomap.bt";
 
   ConciseArgs opt(argc, argv);
   opt.add(radius, "r", "radius");
+  opt.add(xmin, "xmin");
+  opt.add(xmax, "xmax");
+  opt.add(ymin, "ymin");
+  opt.add(ymax, "ymax");
+  opt.add(zmin, "zmin");
+  opt.add(zmax, "zmax");
   opt.add(octomap_fname, "m", "map_name", "full path to map");
   opt.parse();
 
@@ -34,8 +47,9 @@ int main(int argc, char **argv)
   double minX, minY, minZ, maxX, maxY, maxZ;
   ocTree->getMetricMin(minX, minY, minZ);
   ocTree->getMetricMax(maxX, maxY, maxZ);
-  printf("\nmap bounds: [%.2f, %.2f, %.2f] - [%.2f, %.2f, %.2f]  res: %f\n", minX, minY, minZ, maxX, maxY, maxZ,
+  printf("\n Input map bounds: [%.2f, %.2f, %.2f] - [%.2f, %.2f, %.2f]  res: %f\n", minX, minY, minZ, maxX, maxY, maxZ,
       ocTree->getResolution());
+//  printf("\n Output map bounds: [%.2f, %.2f, %.2f] - [%.2f, %.2f, %.2f]\n", xmin, ymin, zmin, xmax, ymax, zmax);
 
   printf("Creating Dilated Map\n");
   float res = ocTree->getResolution();
@@ -50,16 +64,34 @@ int main(int argc, char **argv)
     count++;
 
     point3d center = it.getCoordinate();
-    // Loop through the cube surrounding the current leaf
-    // If the given cell is within a sphere of radius specified above, then fill it
-    // NOTE: CELLS ARE BEING RE-FILLED OVER AND OVER AGAIN BECAUSE THEY FALL WITHIN THE RADIUS OF MULTIPLE CELLS OF THE ORIGINAL MAP
-    // PROBABLY A MUCH FASTER WAY TO DO THIS
-    for (double x = -radius; x <= radius; x += res) {
-      for (double y = -radius; y <= radius; y += res) {
-        for (double z = -radius; z <= radius; z += res) {
-          if (sqrt(x * x + y * y + z * z) <= radius) {
-            point3d newpoint(center(0) + x, center(1) + y, center(2) + z);
-            ocTree_dilated->updateNode(newpoint, true);
+
+    if (xmax != -1 && ymax != -1 && zmax != -1) {
+      if (center(0) > xmin && center(0) < xmax && center(1) > ymin && center(1) < ymax && center(2) > zmin
+          && center(2) < zmax) {
+        // Loop through the cube surrounding the current leaf
+        // If the given cell is within a sphere of radius specified above, then fill it
+        // NOTE: CELLS ARE BEING RE-FILLED OVER AND OVER AGAIN BECAUSE THEY FALL WITHIN THE RADIUS OF MULTIPLE CELLS OF THE ORIGINAL MAP
+        // PROBABLY A MUCH FASTER WAY TO DO THIS
+        for (double x = -radius; x <= radius; x += res) {
+          for (double y = -radius; y <= radius; y += res) {
+            for (double z = -radius; z <= radius; z += res) {
+              if (sqrt(x * x + y * y + z * z) <= radius) {
+                point3d newpoint(center(0) + x, center(1) + y, center(2) + z);
+                ocTree_dilated->updateNode(newpoint, true);
+              }
+            }
+          }
+        }
+      }
+    }
+    else {
+      for (double x = -radius; x <= radius; x += res) {
+        for (double y = -radius; y <= radius; y += res) {
+          for (double z = -radius; z <= radius; z += res) {
+            if (sqrt(x * x + y * y + z * z) <= radius) {
+              point3d newpoint(center(0) + x, center(1) + y, center(2) + z);
+              ocTree_dilated->updateNode(newpoint, true);
+            }
           }
         }
       }
